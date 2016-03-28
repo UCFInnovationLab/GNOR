b8ca0976e264d91992eef10794d7e7bf1aabec35
crabmeat99 <harper@cs.ucf.edu>
2016-03-28 02:41:10 -0400

    delete /debug contents

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"C:/Users/harper/workspace_v6_1/boat/core/mpl/libmplmpu.lib" -l"libc.a"
-
